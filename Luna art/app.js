188a16d3d3a572bafeab32f8d027743a502f6cd9
Vlad <higemaster1@gmail.com>
2023-03-23 11:41:02 +0100

    рт

--- Luna art/app.js
+++ Luna art/app.js
@@ -2,3 +2,28 @@ hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('x');
     nav.classList.toggle('open');
 })
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9raW9ib3kiLCJhIjoiY2w1ODIxMjV2MWRkeTNrcGJtbTdtdnhzciJ9.pPOEquxCJnl4OihAYBsBeQ';
+
+let loc = [139.7583324197658 , 35.79378944095339];
+let map = new mapboxgl.Map({
+    container: 'studio',
+    center: loc,
+    // long,lat
+    zoom: 14,
+    style: 'mapbox://style/mapbox/dark-v10'
+});
+
+map.scrollZoom.disable();   
+map.addControl(new mapboxgl.NavigationControl());
+
+let marker = document.createElement('div');
+marker.id = 'marker';
+
+let popup = new mapboxgl.Popup({offset: 70})
+.setText('div');
+
+new mapboxgl.Marker(marker, {anchor: 'bottom'})
+.setLngLat(loc)
+.addTo(map)
+.setPopup(popup);
